404e5c57a22eb066333fe0456832660ff3d5ac94
sumudu <sumudupriyankara263@gmail.com>
2025-01-29 19:54:30 +0530

    link manager

---
+++ Script/linkManager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class linkManager : MonoBehaviour
+{
+    public void openWhatsapp()
+    {
+        Application.OpenURL("");
+    }
+
+    public void openFacebook()
+    {
+        Application.OpenURL("https://web.facebook.com/sumudu.priyankara.750331");
+    }
+
+    public void openInstergram()
+    {
+        Application.OpenURL("https://www.instagram.com/S_U_M_U_D_U__/?fbclid=IwY2xjawIGr8RleHRuA2FlbQIxMAABHWDLU0uvMZPRsVbj17mWDUX4cuRp23SuiqC-EVDdycSaxoxZ_fIE2DgMng_aem_ALurpQ0H6tn-FU0KKEW5jQ#");
+    }
+}
